d9a014473bbecb2c38ba9a33843f23b953ecb132
lawtonb <lawton.a.browning@gmail.com>
2016-02-17 11:08:25 -0800

    first passing test

--- Objects/FindReplaceTest.cs
+++ Objects/FindReplaceTest.cs
@@ -1,5 +1,5 @@
 using Xunit;
-using Anagrams;
+using FindReplace.Objects;
 using System;
 using System.Collections.Generic;
 namespace  FindReplaceTest
@@ -10,9 +10,10 @@ namespace  FindReplaceTest
     public void IsFindReplace_ForWholeWord_true()
     {
       string input1 = "world";
-      string input2 = "universe";
-      FindReplacer newFindReplacer = new FindReplacer(input1, input2);
-      Assert.Equal(input1, newFindReplacer.CompareInputs());
+      string input2 = "world";
+      string input3 = "universe";
+      FindReplacer newFindReplacer = new FindReplacer(input1, input2, input3);
+      Assert.Equal(input3, newFindReplacer.Replace());
     }
   }
 }
