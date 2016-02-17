a7bde99665cc01d8514a8a9598c67f57702592d8
lawtonb <lawton.a.browning@gmail.com>
2016-02-17 14:13:32 -0800

    all instances test passes

--- Objects/FindReplaceTest.cs
+++ Objects/FindReplaceTest.cs
@@ -6,19 +6,29 @@ namespace  FindReplaceTest
 {
   public class FindReplaceTester
   {
-    // [Fact]
-    // public void IsFindReplace_ForWholeWord_true()
-    // {
-    //   string input1 = "world";
-    //   string input2 = "world";
-    //   string input3 = "universe";
-    //   FindReplacer newFindReplacer = new FindReplacer(input1, input2, input3);
-    //   Assert.Equal(input3, newFindReplacer.Replace());
-    // }
+    [Fact]
+    public void IsFindReplace_ForWholeWord_true()
+    {
+      string input1 = "world";
+      string input2 = "world";
+      string input3 = "universe";
+      FindReplacer newFindReplacer = new FindReplacer(input1, input2, input3);
+      Assert.Equal(input3, newFindReplacer.Result());
+    }
+
     [Fact]
     public void IsFindReplace_ForWholeWordInSentence_true()
     {
       string input1 = "hello world";
+      string input2 = "world";
+      string input3 = "universe";
+      FindReplacer newFindReplacer = new FindReplacer(input1, input2, input3);
+      Assert.Equal(input3, newFindReplacer.Result());
+    }
+
+    public void IsFindReplace_ForWholeAllWordsInSentence_true()
+    {
+      string input1 = "world hello world";
       string input2 = "world";
       string input3 = "universe";
       FindReplacer newFindReplacer = new FindReplacer(input1, input2, input3);
