6d09f93cb4fd64d37661edd54e46c4f3a84c5192
lawton browning <lawton.a.browning@gmail.com>
2016-02-19 11:43:08 -0800

    front end nancy added

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,21 +1,22 @@
-// using Nancy;
-// using RepeatCount.Objects;
-// using System.Collections.Generic;
-// using System;
-//
-// namespace RepeatCounterProject
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => View ["index.cshtml"];
-//       //loads index view at root//
-//
-//       Post["/"] = _ => {
-//       RepeatCounter newRepeatCounter = new RepeatCounter(Request.Form["word"], Request.Form["phrase"]);
-//       return View["results.cshtml", newRepeatCounter];
-//       };
-//     }
-//   }
-// }
+using Nancy;
+using RepeatCount.Objects;
+using System.Collections.Generic;
+using System;
+
+namespace RepeatCounterProject
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => View ["index.cshtml"];
+      //loads index view at root//
+
+      Post["/results"] = _ => {
+      // RepeatCounter newRepeatCounter = new RepeatCounter(Request.Form["word"], Request.Form["phrase"]);
+      // int result = newRepeatCounter.CountRepeats();
+      return View["results.cshtml", RepeatCounter.CountRepeats(Request.Form["word"], Request.Form["phrase"])];
+      };
+    }
+  }
+}
